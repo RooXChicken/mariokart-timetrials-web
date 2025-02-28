0778b10a78c438bfc83298658cca9948b761a8f3
RooXChicken <rooxchicken@gmail.com>
2025-02-27 22:12:35 -0500

    Added the course icon and made it dynamic. Also grab course information depending on which course was selected

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const serverIP = "http://localhost:25532";
+let courses = null;
+let selectedCourse = null;
 
 const courseSelect = document.getElementById("courseSelect");
-let courses = null;
+const courseIcon = document.getElementById("courseIcon");
 
 async function onLoad() {
     let _coursesRaw = await getAPI("course");
     courses = JSON.parse(_coursesRaw);
 
     for(let i = 0; i < 96; i++)
-        courseSelect.appendChild(new Option(courses[i].name))
+        courseSelect.appendChild(new Option(courses[i].name, courses[i].identifier))
 };
 
 async function onSelectionChange() {
-    await getAPI("course?identifier=" + courseSelect.nodeValue);
+    let _courseRaw = await getAPI("course?identifier=" + courseSelect.options[courseSelect.selectedIndex].value);
+    selectedCourse = JSON.parse(_courseRaw)[0];
+
+    courseIcon.src = "assets/icons/" + selectedCourse.identifier + ".png";
 }
 
 async function getAPI(_request) {
